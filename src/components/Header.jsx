3221b5591962e3d70d9d366b8aa861e8be3d85ad
Nilakshhh <97288540+Nilakshhh@users.noreply.github.com>
2022-09-03 15:42:59 +0530

    developing pages

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "../small compo/Image";
+
+function Header() {
+return(
+    <header>
+        <Image src="../images/ieeeusictlogo.png"/>
+        
+    </header>
+)
+}
+
+export default Header;
