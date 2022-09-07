8d028d0e3bca31ea53be3a47caaf22ff68b162ed
Nilakshhh <97288540+Nilakshhh@users.noreply.github.com>
2022-09-07 13:14:10 +0530

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,6 +20,7 @@ import React from "react";
          </li>
        </ul>
        <div className="extrasection">
+       <a href="https://instagram.com/ieeeggsipu?igshid=YmMyMTA2M2Y="><i class="fa-brands fa-4x fa-instagram"></i></a>
        <a href="https://www.linkedin.com/company/ieeeggsipu/"><i class="fa-brands fa-4x fa-linkedin"></i></a>
        </div>
      </div>
