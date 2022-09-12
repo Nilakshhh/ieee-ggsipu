8f775b6648b08cfe8491d3a823d9055a25857da8
Nilakshhh <97288540+Nilakshhh@users.noreply.github.com>
2022-09-12 22:28:36 +0530

    dep

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {
   Routes,
   Route
 } from 'react-router-dom'
-import Home from './pages/Home';
+import Home from './pages/home.jsx';
 import About from './pages/About';
 import Header from './components/Header';
 import Contact from './pages/Contact';
